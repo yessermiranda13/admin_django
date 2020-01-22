490e2a07f83a941d6f38796be5bbd738656a7118
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2020-01-22 08:10:27 -0600

    Administracion de los datos de encuestas

--- mysite/encuestas/urls.py
+++ mysite/encuestas/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #ex: /encuestas/
     path('', views.index, name='index'),
+    #ex: /encuestas/5/
+    path('<int:id_pregunta>/',views.detalle, name='detalle'),
+    #ex: /encuestas/5/resultados/
+    path('<int:id_pregunta>/resultados',views.resultados,name='resultados'),
+    #ex: /encuestas/5/voto/
+    path('<int:id_pregunta>/voto/',views.voto, name='voto'),
 ]
--- mysite/encuestas/views.py
+++ mysite/encuestas/views.py
@@ -2,7 +2,33 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.http import Http404
+from .models import Preguntas
+from django.template import loader
 
-
+#def index(request):
+ #   lista_preguntas_recientes = Preguntas.objects.order_by('-fecha_pub')[:5]
+  #  plantilla = loader.get_template('encuestas/index.html')
+   # context = {
+    #    'lista_preguntas_recientes': lista_preguntas_recientes,
+    #}
+    #return HttpResponse(plantilla.render(context, request))
 def index(request):
-    return HttpResponse("Hola, mundo!! estas en el inicio de encuestas.")
+    lista_preguntas_recientes = Preguntas.objects.order_by('-fecha_pub')[:5]
+    context = {'lista_preguntas_recientes': lista_preguntas_recientes}
+    return render(request, 'encuestas/index.html', context)
+
+def detalle(request, id_pregunta):
+    try:
+        pregunta = Preguntas.objects.get(pk=id_pregunta)
+    except Preguntas.DoesNotExist:
+        raise Http404("Pregunta inexistente")
+    return render(request, 'encuestas/detalle.html', {'pregunta': pregunta})
+    #return HttpResponse("Tu estas viendo la pregunta %s." %id_pregunta)
+
+def resultados(request, id_pregunta):
+    response = "Estas viendo los resultados de la pregunta %s."
+    return HttpResponse(response % id_pregunta)
+
+def voto(request, id_pregunta):
+    return HttpResponse("estas votando por la pregunta %s." %id_pregunta)
